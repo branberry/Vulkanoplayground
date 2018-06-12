63fbdec8be3295e98efe23e5abb3230b1ec9d6a4
wowbob396 <wowbob396@gmail.com>
2018-06-11 21:31:36 -0500

    added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate approx;
+extern crate nalgebra as na;
+
+use na::{Vector3, Rotation3};
+
 fn main() {
-    println!("Hello, world!");
+    /**
+        Simple example using the nalgebra library
+    */
+    let axis = Vector3::x_axis();
+    let angle = 1.57;
+    let b = Rotation3::from_axis_angle(&axis, angle);
+
+    relative_eq!(b.axis().unwrap(), axis);
+    relative_eq!(b.angle(), angle);
+    println!("hello!");
 }
